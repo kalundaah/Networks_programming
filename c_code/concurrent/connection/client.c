fccf9aeb19f7811a3a75333d18e45ec6907198e6
Kalundaah <84059634+kalundaah@users.noreply.github.com>
2024-05-20 23:30:04 +0300

    Refactoring and reorganizing

--- c_code/concurrent/connection/client.c
+++ c_code/concurrent/connection/client.c
@@ -13,7 +13,7 @@
 #define BUFFER_SIZE 1024
 
 void checkMemoryAllocation(void *ptr);
-void handle_server_response(int client_fd);
+// void handle_server_response(int client_fd);
 
 int main(int argc, char const *argv[])
 {
@@ -64,175 +64,150 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    }
+    // Child process to handle server responses
+    // handle_server_response(client_fd);
 
-    if (pid == 0)
+    // Parent process to handle user input
+    char buffer[BUFFER_SIZE] = {0};
+
+    while (1)
     {
-        // Child process to handle server responses
-        handle_server_response(client_fd);
-    }
-    else
-    {
-        // Parent process to handle user input
-        char buffer[BUFFER_SIZE] = {0};
+        // Print menu on client side
+        printf("\n\n====\n");
+        printf("Menu\n");
+        printf("====\n");
+        printf("1. Display Catalog\n");
+        printf("2. Search Book\n");
+        printf("3. Order Book\n");
+        printf("4. Pay for Book\n");
+        printf("\n");
+
         char option[4];
         char params[128];
 
         while (1)
         {
-            // Print menu on client side
-            printf("\n\n====\n");
-            printf("Menu\n");
-            printf("====\n");
-            printf("1. Display Catalog\n");
-            printf("2. Search Book\n");
-            printf("3. Order Book\n");
-            printf("4. Pay for Book\n");
-            printf("\n");
+            printf("Choose Option: ");
+            bzero(option, sizeof(option));
+            fgets(option, sizeof(option), stdin); // get input
+            printf("option: %s", option);
 
-            while (1)
+            int isValidNumber = 1;
+
+            // Checking if number
+            for (int i = 0; i < strlen(option) - 1; i++)
             {
-                printf("Choose Option: ");
-                bzero(option, sizeof(option));
-                fgets(option, sizeof(option), stdin); // get input
-                printf("option: %s", option);
-
-                int isValidNumber = 1;
-
-                // Checking if number
-                for (int i = 0; i < strlen(option) - 1; i++)
+                if (!isdigit(option[i]))
                 {
-                    if (!isdigit(option[i]))
-                    {
-                        isValidNumber = 0;
-                    }
+                    isValidNumber = 0;
                 }
-
-                if (!isValidNumber)
-                {
-                    printf("\nERROR: Please put a number.\n");
-                    continue;
-                }
-
-                // Checking if valid option
-                if (atoi(option) > 4)
-                {
-                    printf("\nERROR: Choose number in the list.\n");
-                    continue;
-                }
-
-                break;
             }
 
-            switch (atoi(option))
+            if (!isValidNumber)
             {
-            case 1:
-                // get query parameters
-                char M[20];
-                char X[20];
-                char Z[20];
-
-                printf("\nDisplay Catalog Parameters\n");
-                printf("-----------------------------");
-                printf("\nM: ");
-                fgets(M, sizeof(M), stdin);
-                printf("X: ");
-                fgets(X, sizeof(X), stdin);
-                printf("Z: ");
-                fgets(Z, sizeof(Z), stdin);
-
-                // Concatenating into one large string
-                snprintf(params, sizeof(params), "%s%s%s", M, X, Z);
-                break;
-            case 2:
-                char string[128];
-                printf("\nSearch Book Parameters\n");
-                printf("-----------------------------");
-                printf("\nstring: ");
-                fgets(string, sizeof(string), stdin);
-
-                // Concatenating into one large string
-                snprintf(params, sizeof(params), "%s", string);
-                break;
-            case 3:
-                // get query parameters
-                char x[20];
-                char y[20];
-                char n[20];
-
-                printf("\nOrder Book Catalog Parameters\n");
-                printf("-----------------------------");
-                printf("\nx: ");
-                fgets(x, sizeof(x), stdin);
-                printf("y: ");
-                fgets(y, sizeof(y), stdin);
-                printf("n: ");
-                fgets(n, sizeof(n), stdin);
-
-                // Concatenating into one large string
-                snprintf(params, sizeof(params), "%s%s%s", x, y, n);
-                break;
-            case 4:
-                char orderno[64];
-                char Amount[64];
-
-                printf("\nOrder Book Catalog Parameters\n");
-                printf("-----------------------------");
-                printf("\norderno: ");
-                fgets(orderno, sizeof(orderno), stdin);
-                printf("Amount: ");
-                fgets(Amount, sizeof(Amount), stdin);
-
-                snprintf(params, sizeof(params), "%s%s", orderno, Amount);
-                break;
-            default:
-                break;
+                printf("\nERROR: Please put a number.\n");
+                continue;
             }
 
-            // clear buffer
-            bzero(buffer, BUFFER_SIZE);
-            snprintf(buffer, BUFFER_SIZE, "%s%s", option, params);
-
-            // send to server
-            if (send(client_fd, buffer, BUFFER_SIZE, 0) < 0)
+            // Checking if valid option
+            if (atoi(option) > 4)
             {
-                perror("ERROR writing to socket");
-                exit(EXIT_FAILURE);
+                printf("\nERROR: Choose number in the list.\n");
+                continue;
             }
 
-            
+            break;
         }
 
-        // Close the client socket when done
-        // close(client_fd);
-    }
+        switch (atoi(option))
+        {
+        case 1:
+            // get query parameters
+            char M[20];
+            char X[20];
+            char Z[20];
 
-    return 0;
-}
+            printf("\nDisplay Catalog Parameters\n");
+            printf("-----------------------------");
+            printf("\nM: ");
+            fgets(M, sizeof(M), stdin);
+            printf("X: ");
+            fgets(X, sizeof(X), stdin);
+            printf("Z: ");
+            fgets(Z, sizeof(Z), stdin);
 
-void handle_server_response(int client_fd)
-{
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
+            // Concatenating into one large string
+            snprintf(params, sizeof(params), "%s%s%s", M, X, Z);
+            break;
+        case 2:
+            char string[128];
+            printf("\nSearch Book Parameters\n");
+            printf("-----------------------------");
+            printf("\nstring: ");
+            fgets(string, sizeof(string), stdin);
+
+            // Concatenating into one large string
+            snprintf(params, sizeof(params), "%s", string);
+            break;
+        case 3:
+            // get query parameters
+            char x[20];
+            char y[20];
+            char n[20];
+
+            printf("\nOrder Book Catalog Parameters\n");
+            printf("-----------------------------");
+            printf("\nx: ");
+            fgets(x, sizeof(x), stdin);
+            printf("y: ");
+            fgets(y, sizeof(y), stdin);
+            printf("n: ");
+            fgets(n, sizeof(n), stdin);
+
+            // Concatenating into one large string
+            snprintf(params, sizeof(params), "%s%s%s", x, y, n);
+            break;
+        case 4:
+            char orderno[64];
+            char Amount[64];
+
+            printf("\nOrder Book Catalog Parameters\n");
+            printf("-----------------------------");
+            printf("\norderno: ");
+            fgets(orderno, sizeof(orderno), stdin);
+            printf("Amount: ");
+            fgets(Amount, sizeof(Amount), stdin);
+
+            snprintf(params, sizeof(params), "%s%s", orderno, Amount);
+            break;
+        default:
+            break;
+        }
+
+        // clear buffer
         bzero(buffer, BUFFER_SIZE);
+        snprintf(buffer, BUFFER_SIZE, "%s%s", option, params);
+
+        // send to server
+        if (send(client_fd, buffer, BUFFER_SIZE, 0) < 0)
+        {
+            perror("ERROR writing to socket");
+            exit(EXIT_FAILURE);
+        }
+
+        bzero(buffer, BUFFER_SIZE);
+        
         int bytes_received = read(client_fd, buffer, BUFFER_SIZE);
         if (bytes_received < 0)
         {
             perror("ERROR reading from socket");
             exit(EXIT_FAILURE);
         }
-        else if (bytes_received == 0)
-        {
-            printf("Server closed the connection\n");
-            kill(getppid(), SIGINT); // Signal parent to exit
-            exit(EXIT_SUCCESS);
-        }
+        // else if (bytes_received == 0)
+        // {
+        //     printf("Server closed the connection\n");
+        //     exit(EXIT_SUCCESS);
+        // }
         else
         {
             printf("+++++++\n");
@@ -241,7 +216,41 @@ void handle_server_response(int client_fd)
             printf("\n%s", buffer);
         }
     }
+
+
+    // Close the client socket when done
+    close(client_fd);
+
+    return 0;
 }
+
+// void handle_server_response(int client_fd)
+// {
+//     char buffer[BUFFER_SIZE];
+//     while (1)
+//     {
+//         bzero(buffer, BUFFER_SIZE);
+//         int bytes_received = read(client_fd, buffer, BUFFER_SIZE);
+//         if (bytes_received < 0)
+//         {
+//             perror("ERROR reading from socket");
+//             exit(EXIT_FAILURE);
+//         }
+//         else if (bytes_received == 0)
+//         {
+//             printf("Server closed the connection\n");
+//             kill(getppid(), SIGINT); // Signal parent to exit
+//             exit(EXIT_SUCCESS);
+//         }
+//         else
+//         {
+//             printf("+++++++\n");
+//             printf("RESULTS\n");
+//             printf("+++++++\n");
+//             printf("\n%s", buffer);
+//         }
+//     }
+// }
 
 void checkMemoryAllocation(void *ptr)
 {
